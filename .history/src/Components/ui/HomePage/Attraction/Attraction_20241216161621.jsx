ea5a9ee11d7c1a8887c402c3840ab933d66e1a15
gonov <adamej10@gmail.com>
2024-12-16 17:01:47 +0300

    3

---
+++ .history/src/Components/ui/HomePage/Attraction/Attraction_20241216161621.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import classes from './Attraction.module.css';
+import { useNavigate } from 'react-router-dom';
+
+function Attraction({ children, ...props }) {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className={classes.container}>
+        <img src="/images/VeloLogo.png" />
+        <img src="/images/VeloName.png" />
+        <img src="/images/Velik.png" />
+        <div className={classes.ContainerBottom}>
+          <div className={classes.ContainerBottom1}>
+            <span>Всероссийский фестиваль «Притяжение»  </span>
+          </div>
+          <div className={classes.ContainerBottom2}>
+            <span>
+            Фирменный стиль Всероссийского фестиваля «Притяжение»
+            </span>
+            <button onClick={() => navigate('/cases')}>ЛОГОТИП И ФИРСТИЛЬ</button>
+            <button onClick={() => navigate('/cases')}>WEB-ДИЗАЙН</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Attraction;
---
+++ .history/src/Components/ui/HomePage/Attraction/Attraction_20241216164848.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import classes from './Attraction.module.css';
+import { useNavigate } from 'react-router-dom';
+
+function Attraction({ children, ...props }) {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className={classes.container}>
+        <img src="/images/baloon.png" />
+        <img src="/images/baloon.png" />
+        src/Components/ui/HomePage/Attraction/Attraction.jsx
+        <div className={classes.ContainerBottom}>
+          <div className={classes.ContainerBottom1}>
+            <span>Всероссийский фестиваль «Притяжение» </span>
+          </div>
+          <div className={classes.ContainerBottom2}>
+            <span>Фирменный стиль Всероссийского фестиваля «Притяжение»</span>
+            <div>
+              <button onClick={() => navigate('/cases')}>
+                ЛОГОТИП И ФИРСТИЛЬ
+              </button>
+              <button onClick={() => navigate('/cases')}>WEB-ДИЗАЙН</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Attraction;
